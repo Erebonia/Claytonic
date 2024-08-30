080c4932e9ab83c337d616ea3f381aa258571255
deathhopes <52137104+Erebonia@users.noreply.github.com>
2024-08-29 16:10:42 -1000

    Data Recovery Screen / UI Revamp

--- DIT AIO/Properties/Resources.Designer.cs
+++ DIT AIO/Properties/Resources.Designer.cs
@@ -143,6 +143,16 @@ namespace DIT_AIO.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap free_data_recovery {
+            get {
+                object obj = ResourceManager.GetObject("free-data-recovery", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap Home {
             get {
                 object obj = ResourceManager.GetObject("Home", resourceCulture);
